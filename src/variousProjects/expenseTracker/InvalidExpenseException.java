9172ab8c5bb748ab7d32f190d8302eb0361082fb
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-02-18 21:34:34 +0200

    custom exception class added

---
+++ src/variousProjects/expenseTracker/InvalidExpenseException.java
@@ -0,0 +1,16 @@
+package variousProjects.expenseTracker;
+
+/**
+ * Custom exception to handle cases where expense data
+ * doesn't meet our business logic requirements.
+ */
+public class InvalidExpenseException extends Exception {
+
+    public InvalidExpenseException(String message) {
+        super(message);
+    }
+
+    public InvalidExpenseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
